628d273f304853e9e6de5efdc2ce8d0881a663ab
Artur Kuznetsov <arturkuz.dkt@gmail.com>
2024-12-26 10:00:53 +0300

    update

--- students/k3340/Kuznetsov_Artur/Lr4/newspaper_distribution_project/vue-project/src/services/axiosClient.js
+++ students/k3340/Kuznetsov_Artur/Lr4/newspaper_distribution_project/vue-project/src/services/axiosClient.js
@@ -1,14 +1,11 @@
-// src/services/axiosClient.js
 import axios from 'axios'
-import { tokenStore } from '@/stores/tokenStore'
+import {tokenStore} from '@/stores/tokenStore'
 
-// Можно указать URL вашей API в переменную окружения или непосредственно
-// baseURL: 'http://127.0.0.1:8000'
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000',
+  baseURL: 'http://127.0.0.1:8000',
 })
 
-// Добавляем перехватчик запросов (request interceptor)
+
 axiosClient.interceptors.request.use((config) => {
   const store = tokenStore()
   if (store.token) {
@@ -18,15 +15,5 @@ axiosClient.interceptors.request.use((config) => {
 }, (error) => {
   return Promise.reject(error)
 })
-
-// Можно также добавить перехватчик ответов (response interceptor)
-// для глобальной обработки ошибок
-axiosClient.interceptors.response.use(
-  response => response,
-  error => {
-    // Тут можно обрабатывать 401, 403 и т.д.
-    return Promise.reject(error)
-  }
-)
 
 export default axiosClient
